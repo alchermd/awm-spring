03d656c6e02c773e78d3d9505f2cf189be651aa3
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-04-06 17:15:17 +0800

    Create prototype for index page

---
+++ web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Print("Starting application.")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set.")
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.ParseFiles("views/index.html"))
+		t.Execute(w, nil)
+	}).Methods("GET")
+
+	assetPath := "/assets/"
+	log.Print("Serving static files on " + assetPath)
+	r.PathPrefix(assetPath).Handler(http.StripPrefix(assetPath, http.FileServer(http.Dir("assets/"))))
+
+	log.Print("Starting server on :" + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
