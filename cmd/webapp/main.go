b8753bf5459022327955fd61db6988356292995b
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-04-07 10:46:26 +0800

    Restructure to follow the Go standard layout

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -19,13 +19,13 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t := template.Must(template.ParseFiles("views/index.html"))
+		t := template.Must(template.ParseFiles("web/templates/index.html"))
 		t.Execute(w, nil)
 	}).Methods("GET")
 
 	assetPath := "/assets/"
 	log.Print("Serving static files on " + assetPath)
-	r.PathPrefix(assetPath).Handler(http.StripPrefix(assetPath, http.FileServer(http.Dir("assets/"))))
+	r.PathPrefix(assetPath).Handler(http.StripPrefix(assetPath, http.FileServer(http.Dir("web/assets/"))))
 
 	log.Print("Starting server on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
