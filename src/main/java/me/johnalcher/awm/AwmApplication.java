bdbd4caeecf70d4028501266b56a0e10accdbdb9
John Alcher M. Doliras <johnalcherdoloiras@gmail.com>
2020-04-10 19:15:48 +0800

    Initial commit for Spring app

---
+++ src/main/java/me/johnalcher/awm/AwmApplication.java
@@ -0,0 +1,13 @@
+package me.johnalcher.awm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/johnalcher/awm/AwmApplicationTests.java
@@ -0,0 +1,13 @@
+package me.johnalcher.awm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
